5b5168eee06f8fddb7965f789ff4c803244c8da5
Martin Braun <martinbraun123@aol.com>
2016-03-26 17:43:41 +0100

    Initial Commit.

---
+++ ps2pdf.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# ----------------------------------------------------------------------------
+# "THE BEER-WARE LICENSE" (Revision 42):
+# <martinbraun123@aol.com> wrote this file. As long as you retain this notice you
+# can do whatever you want with this stuff. If we meet some day, and you think
+# this stuff is worth it, you can buy me a beer in return Poul-Henning Kamp
+# ----------------------------------------------------------------------------
+cp "%1" "%1_org.ps"
+ps2eps -f "%1_org.ps"
+epstool --copy --bbox --output "%1_pre_pdf.eps" "%1_org.eps"
+epstopdf "%1_pre_pdf.eps" --gsopt=-dEPSCrop --exact --outfile="%2"
+rm "%1_org.ps"
+rm "%1_org.eps"
+sleep 1
+rm "%1_pre_pdf.eps"
